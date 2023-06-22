b5163f9b7b4a3cfb1708bbc3a140bfa6d73f8030
Micle Dev <mickle.develop@gmail.com>
2023-06-22 14:06:57 +0300

    learn "Drawer" navigation option - creates list with our screens hidden to the left of screens, can be opened by pressing menu buttons. Learn "bottom tabs" navigation list that is placed as text and icons on botton of the screen, pressing on each of them navigates in our app

--- App.js
+++ App.js
@@ -1,7 +1,92 @@
-import { StyleSheet, Text, View } from "react-native";
+import "react-native-gesture-handler";
+
+import { StyleSheet } from "react-native";
+
+import { StatusBar } from "expo-status-bar";
+
+import { NavigationContainer } from "@react-navigation/native";
+
+// import { createDrawerNavigator } from "@react-navigation/drawer";
+
+// import MyTabBar from "./screens/MyTab";
+
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+
+import { Ionicons } from "@expo/vector-icons";
+
+import WelcomeScreen from "./screens/WelcomeScreen";
+import UserScreen from "./screens/UserScreen";
+
+// const Drawer = createDrawerNavigator();
+
+const BottomTab = createBottomTabNavigator();
 
 export default function App() {
-  return <Text>Todo...</Text>;
+  return (
+    <>
+      <StatusBar style="light" />
+      <NavigationContainer>
+        {/* <Drawer.Navigator
+          screenOptions={{
+            drawerActiveBackgroundColor: "#f0e1ff",
+            drawerActiveTintColor: "#3c0a6b",
+            headerStyle: { backgroundColor: "#3c0a6b" },
+            headerTintColor: "white",
+            drawerType: 'slide',
+            // drawerStyle: { backgroundColor: "#ccc" },
+          }}
+        >
+          <Drawer.Screen
+            name="Welcome"
+            component={WelcomeScreen}
+            options={{
+              drawerLabel: "Welcome drawer",
+              drawerIcon: ({ color, size }) => (
+                <Ionicons name="home" color={color} size={size} />
+              ),
+              // lazy: false,
+            }}
+          />
+          <Drawer.Screen
+            name="User"
+            component={UserScreen}
+            options={{
+              drawerIcon: ({ color, size }) => (
+                <Ionicons name="person" color={color} size={size} />
+              ),
+            }}
+          />
+        </Drawer.Navigator> */}
+        <BottomTab.Navigator
+          // tabBar={(props) => <MyTabBar {...props} />}
+          screenOptions={{
+            headerStyle: { backgroundColor: "#3c0a6b" },
+            headerTintColor: "white",
+            tabBarActiveTintColor: '#3c0a6b',
+          }}
+        >
+          <BottomTab.Screen
+            name="Welcome"
+            component={WelcomeScreen}
+            options={{
+              tabBarIcon: ({ color, size }) => (
+                <Ionicons color={color} size={size} name="home" />
+              ),
+            }}
+          />
+          <BottomTab.Screen
+            name="User"
+            component={UserScreen}
+            options={{
+              tabBarIcon: ({ color, size }) => (
+                <Ionicons color={color} size={size} name="person" />
+              ),
+            }}
+          />
+        </BottomTab.Navigator>
+      </NavigationContainer>
+    </>
+  );
 }
 
 const styles = StyleSheet.create({
